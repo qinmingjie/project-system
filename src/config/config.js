4ee97287b2748da6ba511a21a7f37d9fbe070792
qinmingjie <1934394876@qq.com>
2023-10-19 14:27:06 +0800

    chore(config): 导出环境变量

--- src/config/config.js
+++ src/config/config.js
@@ -5,13 +5,23 @@ const dotenv = require("dotenv");
 
 // 获取.env配置的环境变量
 dotenv.config();
-const { APP_PORT, MYSQL_HOST, MYSQL_PORT, MYSQL_USER, MYSQL_DATABASE, MYSQL_PASSWORd, AVATAR_PATH, PICTURE_PATH } =
-  process.env;
+const {
+  APP_HOST,
+  APP_PORT,
+  MYSQL_HOST,
+  MYSQL_PORT,
+  MYSQL_USER,
+  MYSQL_DATABASE,
+  MYSQL_PASSWORd,
+  AVATAR_PATH,
+  PICTURE_PATH
+} = process.env;
 
 const PUBLIC_KEY = fs.readFileSync(path.join(__dirname, "./public.key"));
 const PRIVATE_KEY = fs.readFileSync(path.join(__dirname, "./private.key"));
 
 module.exports = {
+  APP_HOST,
   APP_PORT,
   MYSQL_HOST,
   MYSQL_PORT,
