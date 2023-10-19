5019e7d87ec5e69db76ad2cce09274cbc8d741d5
qinmingjie <1934394876@qq.com>
2023-10-19 11:00:52 +0800

    chore(userRouter): 修改user接口，添加前缀

--- src/router/module/user.js
+++ src/router/module/user.js
@@ -2,9 +2,11 @@ const Router = require("@koa/router");
 const { registerUserVerify, cryptoPassword, loginVerify } = require("../../middleware/user.middle");
 const { createUser, loginToken } = require("../../control/user.control");
 
-const userRouter = new Router();
+const userRouter = new Router({ prefix: "/user" });
 
-userRouter.post("/user", registerUserVerify, cryptoPassword, createUser);
+// 注册
+userRouter.post("/register", registerUserVerify, cryptoPassword, createUser);
+// 登录
 userRouter.post("/login", loginVerify, loginToken);
 
 module.exports = userRouter;
